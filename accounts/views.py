46dd1d60e6308c6ba8bf36336094009cb9bb1777
Hugo Brito <hugomdmb@gmail.com>
2023-04-07 17:14:48 -0300

    aula S3-12 finaliada

--- accounts/views.py
+++ accounts/views.py
@@ -8,4 +8,14 @@ from .models import Account
 
 class AccountFindView(APIView):
     def get(self, request):
-        ...
+        account_balance = request.query_params.get('account_balance_gte', None)
+        gender = request.query_params.get('gender', None)
+
+        accounts = Account.objects.filter(
+            account_balance__gte=account_balance,
+            gender=gender
+        )
+
+        serializer = AccountSerializer(accounts, many=True)
+
+        return Response(serializer.data)
